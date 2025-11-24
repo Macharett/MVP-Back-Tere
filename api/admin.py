e8e914ef0e5a150faefc87ccc9c31319c0b4f9c1
Anderson Macharett <165483617+Macharett@users.noreply.github.com>
2025-11-24 19:11:04 -0300

    Registrando models no admin django.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Parque, Trilha, Evento, Biodiversidade
 
 # Register your models here.
+@admin.register(Parque)
+class ParqueAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'altitude_media', 'horario_funcionamento')
+    search_fields = ('nome',)
+
+@admin.register(Trilha)
+class TrilhaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'parque', 'dificuldade', 'distancia_km', 'status_aberta')
+    list_filter = ('parque', 'dificuldade', 'status_aberta')
+    search_fields = ('nome', 'descricao')
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'parque', 'data_inicio', 'disponibilidade')
+    list_filter = ('parque', 'data_inicio')
+    date_hierarchy = 'data_inicio'
+    search_fields = ('titulo', 'descricao')
+
+@admin.register(Biodiversidade)
+class BiodiversidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome_comum', 'nome_cientifico', 'categoria', 'parque', 'status_conservacao')
+    list_filter = ('categoria', 'status_conservacao', 'parque')
+    search_fields = ('nome_comum', 'nome_cientifico', 'descricao')
+    ordering = ('nome_comum',)
