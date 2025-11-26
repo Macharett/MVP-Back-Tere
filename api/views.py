e3d8225a8719f844d939b7519d217dfaf450e580
Carlos Vinicios <carlos.feso.ti@gmail.com>
2025-11-26 17:04:33 -0300

    Criando as class em view

--- api/views.py
+++ api/views.py
@@ -4,6 +4,26 @@ from django.utils import timezone
 from .models import Parque
 from .serializers import ParqueSerializer
 
+
 class ParqueViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Parque.objects.all()
     serializer_class = ParqueSerializer
+
+
+class TrilhaViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Trilha.objects.filter(status_aberta=True)
+    serializer_class = TrilhaSerializer
+    filterset_fields = ['parque', 'dificuldade']
+
+
+class EventoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Evento.objects.filter(
+        Q(data_fim__isnull=True) | Q(data_fim__gte=timezone.now())
+    )
+    serializer_class = EventoSerializer
+    ordering_fields = ['data_inicio']
+    filterset_fields = ['parque']
+
+
+class BiodiversidadeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = 
