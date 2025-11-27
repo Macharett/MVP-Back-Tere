a3dc30df6da6cf4b32e416f2fd12989f391c98ac
Anderson Macharett <165483617+Macharett@users.noreply.github.com>
2025-11-26 21:49:14 -0300

    Atualizando Databases em settings.py

--- tereverde/settings.py
+++ tereverde/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tereverde.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'MbvrqOlwOtkqTgvZosnQjAuGEjRjcFar',
+        'HOST': 'maglev.proxy.rlwy.net',
+        'PORT': '45730',
     }
 }
 
