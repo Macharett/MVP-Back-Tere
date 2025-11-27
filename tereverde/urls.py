6302ee25e99ae37ce6bd95fdf555d50eb910ebf4
Anderson Macharett <165483617+Macharett@users.noreply.github.com>
2025-11-27 19:39:05 -0300

    Redirecionando urls home para admin

--- tereverde/urls.py
+++ tereverde/urls.py
@@ -5,7 +5,12 @@ admin.site.site_header = "Tere Verde Administração"
 admin.site.site_title = "Tere Verde Admin Portal" 
 admin.site.index_title = "Bem-vindo ao Portal de Administração Tere Verde"
 
+def redirect_to_admin(request):
+    return redirect('/admin/')
+
+
 urlpatterns = [
+    path('', redirect_to_admin),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
